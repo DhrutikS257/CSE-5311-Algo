36c02ea52559ba79aeef385988f8ceb79c37a3c0
Dhrutik <solankdhrutik0602@gmail.com>
2025-02-18 21:21:28 -0600

    Feat: added pop_head, template to allow any datatype

--- HW5/src/min_heap.cpp
+++ HW5/src/min_heap.cpp
@@ -1,75 +1,103 @@
-#include <iostream> 
+#include <iostream>
 #include <vector>
 
 using namespace std;
-
-int parent(int i ) {
-    return (i-1)/2;
+int parent(int i) {
+    return (i - 1) >> 1;
 }
 
+
 int left(int i) {
-    return 2*i;
+    return (i << 1) + 1;
 }
 
 int right(int i) {
-    return 2*i + 1;
+    return (i << 1) + 2;
 }
 
-struct node
-{
-    int key;
-    int data;
-};
 
-
-void min_heapify(vector<int>& arr, int i) {
-    int smallest;
+template <typename T>
+void min_heapify(vector<T>& heap, int i, int n) {
     int l = left(i);
     int r = right(i);
+    int smallest = i;
 
-    if (l <= arr.size() && arr[l] < arr[i])
+    if (l < n && heap[l] < heap[smallest])
         smallest = l;
-    else
-        smallest = i;
-    
-    if (r <= arr.size() && arr[r] < arr[smallest])
+    if (r < n && heap[r] < heap[smallest])
         smallest = r;
-    
     if (smallest != i) {
-        swap(arr[i], arr[smallest]);
-        min_heapify(arr, smallest);
+        swap(heap[i], heap[smallest]);
+        min_heapify(heap, smallest, n);
     }
 }
 
-vector<node> build_nodes(vector<int>& arr) {
-    vector<node> nodes;
-    for (int i = 0; i < arr.size(); i++) {
-        node n;
-        n.key = arr[i];
-        n.data = arr[i];
-        nodes.push_back(n);
+
+template <typename T>
+void build_min_heap(vector<T>& heap) {
+    int n = heap.size();
+
+    for (int i = parent(n - 1); i >= 0; --i) {
+        min_heapify(heap, i, n);
     }
-    return nodes;
 }
 
-vector<node> build_min_heap(vector<node>& arr){
-    
+
+template <typename T>
+T top_heap(const vector<T>& heap) {
+    if (heap.empty())
+        throw runtime_error("Heap is empty");
+    return heap.front();
 }
 
+template <typename T>
+T pop_heap(vector<T>& heap) {
+    if (heap.empty())
+        throw runtime_error("Heap is empty");
+    T root = heap.front();
+    heap.front() = heap.back();
+    heap.pop_back();
+    min_heapify(heap, 0, heap.size());
+    return root;
+}
+
+
+template <typename T>
+void print_heap(const vector<T>& heap) {
+    for (const auto& item : heap)
+        cout << item << " ";
+    cout << "\n";
+}
 
 int main() {
-    vector<int> arr1 = {27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0};
+
+    vector<int> data = {27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0};
 
     cout << "Original array: ";
-    for (int i = 0; i < arr1.size(); i++)
-        cout << arr1[i] << " ";
+    for (int d : data)
+        cout << d << " ";
+    cout << "\n";
 
-    for (int i = arr1.size()/2; i >= 0; i--)
-        min_heapify(arr1, i);
 
-    cout << "\nMin-Heap after heapify operation: ";
-    for (int i = 0; i < arr1.size(); i++)
-        cout << arr1[i] << " ";
+    build_min_heap(data);
+    cout << "Heap array after build_min_heap(): ";
+    print_heap(data);
+
+
+    cout << "\nRemoving elements from the heap:\n";
+    while (!data.empty()) {
+        try {
+            int top = top_heap(data);
+            cout << "Top element: " << top << "\n";
+            int popped = pop_heap(data);
+            cout << "Popped element: " << popped << "\n";
+            cout << "Heap now: ";
+            print_heap(data);
+        } catch (const runtime_error& e) {
+            cout << "Heap is empty!\n";
+            break;
+        }
+    }
 
     return 0;
 }
